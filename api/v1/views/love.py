4c846748404893a56731e352ad9d15b2500c0d99
Noeuclides <euclidesnoeuclides@gmail.com>
2019-10-04 22:33:17 +0000

    [feature] get auth_token from student

--- api/v1/views/love.py
+++ api/v1/views/love.py
@@ -3,16 +3,29 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 import requests
-
+import json
 
 
 @app_views.route('/')
 def root():
     return jsonify({"Hola": "OK"})
 
+@app_views.route('/auth_token')
+def get_auth_token():
+    student = {
+        "api_key": request.form.get('api_key'),
+        "email": request.form.get('email'),
+        "password": request.form.get('password'),
+        "scope": "checker"
+    }
+
+    res = requests.post('https://intranet.hbtn.io/users/auth_token.json', data=student)
+
+    return jsonify(token= res.json()["auth_token"])
+
 @app_views.route('/love_path_photo')
 def love_path_photo():
-    auth_token = {"auth_token": "1b565e64dcf1e8ca5ec38900ca29d15a7871e65cf2bcb71caad153609e1c6d67"}
+    auth_token = {"auth_token": token}
     headers = {'Content-Type': 'application/json'}
     res = requests.get('https://intranet.hbtn.io/users/me.json', params=auth_token, headers=headers)
     return res.json()
