a338b9ffa67e71fe7f6a73c87c01377067435e6a
nildiert <niljordan23@gmail.com>
2019-10-05 08:19:33 -0500

    [FEAUTURE] Added authentication page

--- static/js/login.js
+++ static/js/login.js
@@ -1,14 +1,32 @@
 $(function () {
+        var height = $(window).height();
+        $('#left-image').height(height);
 
-    $.ajax({
-        type: 'GET',
-        url: 'http://127.0.0.1:5001/api/v1/',
-        data: JSON.stringify({
-        }),
-        contentType: 'application/json',
-        success: function (data) {
-            console.log(data);
-            
+    $("#send").click(function () {
+        email = $("#email").val()
+        password = $("#password").val()
+        api_key = $("#apikey").val()
+        if (email && password && api_key) {
+            $.ajax({
+                type: 'POST',
+                crossDomain: true,
+                datatype: 'jsonp',
+                url: 'http://127.0.0.1:5001/api/v1/auth_token',
+                data: JSON.stringify({
+                    "email": email,
+                    "password": password,
+                    "api_key": api_key
+                }),
+                header: {
+                    "Access-Control-Allow-Headers" : "X-PINGOTHER"
+                },
+                contentType: 'application/json',
+                success: function (data) {
+                    console.log(data);   
+                }
+            });
         }
+
     });
+
 })
