e28e4c4e41f7ab6972ed5f302881ab8061b49d8d
nildiert <niljordan23@gmail.com>
2019-10-05 00:00:25 -0500

    [FEATURE] Added files js

--- static/js/login.js
+++ static/js/login.js
@@ -1,14 +1,26 @@
 $(function () {
-
-    $.ajax({
-        type: 'GET',
-        url: 'http://127.0.0.1:5001/api/v1/',
-        data: JSON.stringify({
-        }),
-        contentType: 'application/json',
-        success: function (data) {
-            console.log(data);
-            
-        }
+    var height = $(window).height();
+    $('#left-image').height(height);
+    alert("Si sirve esta mierda")
+    $("#send").click(function () {
+        console.log("Enviando")
+        $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:5001/api/v1/auth_token',
+            data: JSON.stringify({
+                email = "730@holbertonschool.com",
+                api_key = "20377af8fe61e278d8113bdcccd118d9",
+                password = "kmzwa8awaa.Nba"
+            }),
+            contentType: 'application/json',
+            success: function (data) {
+                console.log("sdfsdf");
+                
+            },
+            failure: function (data) {
+                console.log(data)
+            }
+        });
     });
+
 })
