c1d1eb85eb430a7aff9ce6d6b5dfede396f576b5
Fesus Rocuts <fesusrocuts@gmail.com>
2019-10-06 00:26:43 +0000

    update front and back flow, burned data as example, change coming soon

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-from flask import Flask, render_template, url_for
+import os.path
 import uuid
-
+import random
+from flask import Flask, request, render_template, \
+    json, jsonify, redirect, url_for
 
 # flask setup
 app = Flask(__name__)
@@ -12,14 +14,44 @@ app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
 
+@app.route('/zonematchramdom', methods=['GET', 'POST'])
+def zonematchramdom():
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    data = request.get_json(silent=True)
+    print("---------")
+    print(data)
+    print("---------")
+    data = {"name":"Test", "lastname": "Test", "image":"https://avatars3.githubusercontent.com/u/2479899?s=460&v=4", "token": "testtoken"}
+    return render_template('card.html', otherdata=data)
 
 
-@app.route('/login')
-def login(the_id=None):
+@app.route('/zonematch', methods=['GET', 'POST'])
+def zonematch():
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    data = request.get_json(silent=True)
+    print("---------")
+    print(data)
+    print("---------")
+    data = {"name":"Test", "lastname": "Test", "image":"https://avatars3.githubusercontent.com/u/2479899?s=460&v=4", "token": "testtoken"}
+    return render_template('play.html', data=data)
+
+
+@app.route('/login', methods=['GET'])
+def login():
     """
     handles request to custom template with states, cities & amentities
     """
     return render_template('login.html')
+
+@app.route('/', methods=['GET'])
+def index():
+    data = {"token":"", "cache_id":uuid.uuid4(),"title":"Sing In to play!"}
+    return render_template('index.html', data=data)
+
 
 if __name__ == "__main__":
     """
--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,29 @@
-$(function () {
-        var height = $(window).height();
-        $('#left-image').height(height);
+//$(function () {
+//var height = $(window).height();
+//$('#left-image').height(height);
 
-    $("#send").click(function () {
+    function loadzonematch(pdata) {
+      console.log("loadzonematch > ");
+      console.log(pdata);
+      $.post("zonematch", pdata, function (data, status) {
+        $("body > .container").html(data);
+	loadzonematchsec2(pdata)
+      });
+    }
+
+    function loadzonematchsec2(pdata) {
+      console.log("loadzonematchsec2 > ");
+      console.log(pdata);
+      $.post("zonematchramdom", pdata, function (data, status) {
+        $(".card").html(data);
+        setTimeout(function(){
+          $(".btn-nonlike img").click(function(){$(".card").html("Loading..."); loadzonematchsec2(pdata);});
+          $(".btn-like img").click(function(){$(".card").html("Loading...");loadzonematchsec2(pdata);});
+        },1000);
+      });
+    }
+
+    function checkloginform() {
         email = $("#email").val()
         password = $("#password").val()
         api_key = $("#apikey").val()
@@ -23,10 +44,36 @@ $(function () {
                 contentType: 'application/json',
                 success: function (data) {
                     console.log(data);   
+		    loadzonematch(data);
                 }
             });
         }
+    }
+    function checkapi(token,uri) {
+            $.ajax({
+                type: 'POST',
+                crossDomain: true,
+                datatype: 'jsonp',
+                url: 'http://127.0.0.1:5001/api/v1/' + uri,
+                data: JSON.stringify({
+                    "token": token
+                }),
+                header: {
+                    "Access-Control-Allow-Headers" : "X-PINGOTHER"
+                },
+                contentType: 'application/json',
+                success: function (data) {
+                    console.log(data);
+                }
+            });
+    }
+    window.onload = function() {
+      $.get("login", function (data, status) {
+        $(".card").html(data);
+        setTimeout(function(){
+          $("#send").click(checkloginform);
+	},1000);
+      });
+    }
 
-    });
-
-})
+//})
